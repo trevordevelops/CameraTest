a0eb7a3aced716bdffc69845de404f74e8ebd85f
Trevor <85640211+trevordevelops@users.noreply.github.com>
2022-02-16 14:25:10 -0800

    Initial Commit

---
+++ CameraTest/CameraTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraTestApp.swift
+//  CameraTest
+//
+//  Created by Trevor Welsh on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct CameraTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
